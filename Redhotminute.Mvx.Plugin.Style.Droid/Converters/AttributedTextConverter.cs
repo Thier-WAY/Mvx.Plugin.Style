6cd3ef0c7b17417d1caefa8a8ea35140bcda69f9
Thierry Noorbergen <thierry.noorbergen@weareyou.com>
2020-08-24 15:31:34 +0200

    Updated Android support packages to AndroidX

--- Redhotminute.Mvx.Plugin.Style.Droid/Converters/AttributedTextConverter.cs
+++ Redhotminute.Mvx.Plugin.Style.Droid/Converters/AttributedTextConverter.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using Android.Content;
-using Android.Graphics;
 using Android.Text;
 using Android.Text.Style;
 using MvvmCross.Binding;
